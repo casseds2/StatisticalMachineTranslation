6340666d5be06a8a48bd42c20e614288f34dd86a
casseds2 <casseds95@gmail.com>
2017-02-21 14:07:42 +0000

    First Commit

---
+++ lab3/BleuCalculator.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class BleuCalculator{
+	public static void main(String [] args){
+		ArrayList<String> transList = new ArrayList<>();
+		ArrayList<String> refList = new ArrayList<>();
+		NGram ngram = new NGram();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String trans = "";
+		String [] transA = null;
+		String [] refA = null;
+		String ref = "";
+		float score = 0;
+		float brevity = 0;
+		try{
+			System.out.println("Enter 2 Sentences To Be Compared");
+			trans = in.readLine();
+			transA = trans.split(" ");
+			ref = in.readLine();
+			refA = ref.split(" ");
+			in.close();
+		}
+		catch(Exception e){
+			System.out.println("Error Reading Input");
+		}
+
+		for(int i = 1; i < 5; i++){
+			transList = ngram.nGram(transA, i);
+			refList = ngram.nGram(refA, i);
+			if(score != 0)
+				score = score * calculator(transList, refList);
+			else
+				score = calculator(transList, refList);
+		}
+		brevity = transList.size()/refList.size();
+		System.out.println("Final Score: " + root(score, 4)/brevity);
+	}
+
+	static float calculator(ArrayList<String> trans, ArrayList<String> ref){
+		float count = 0;
+		for(int i = 0; i < trans.size(); i++){
+			if(trans.get(i).equals(ref.get(i)))
+				count++;	
+		}
+		return count/ref.size();
+	}
+
+	public static double root(double num, double root) {
+	    return Math.pow(Math.E, Math.log(num)/root);
+	}    
+}
---
+++ lab3/NGram.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class NGram{
+	public static void main(String [] args){
+
+		String sample = "cat sat on the mat";
+
+		String [] tokens = sample.split(" ");
+		int n = 3;
+
+		ArrayList <String> list = new ArrayList<>();
+		list = nGram(tokens, n);
+		for(String s : list){
+			System.out.print(s + ",");
+		}
+		System.out.println();
+	}
+
+	NGram(){}
+
+	/**Function Does For Any number of N**/
+	static ArrayList<String> nGram(String [] tokens, int n){
+		ArrayList<String> list = new ArrayList<>();
+		for(int i = 0; i < tokens.length-(n-1); i++){
+			int position = i;
+			int count = 0;
+			while(count < n){
+				list.add(tokens[position]);
+				count++;
+				position++;
+			}
+			count = 0;
+		}
+		return list;
+	}
+
+	static void oneGram(String [] tokens){
+		System.out.println("//////////////");
+		System.out.println("1 Gram");
+		for(int i = 0; i < tokens.length; i++){
+			System.out.print(tokens[i] + " ");
+		}
+		System.out.println();
+	}
+
+	static void twoGram(String [] tokens){
+		System.out.println("//////////////");
+		System.out.println("2 Gram");
+		System.out.print("{");
+		for(int i = 0; i < tokens.length-1; i++){
+			System.out.print(tokens[i] + ", " + tokens[i+1] + "," + " ");
+		}
+		System.out.print("}");
+		System.out.println();
+	}
+
+	static void threeGram(String [] tokens){
+		System.out.println("//////////////");
+		System.out.println("3 Gram");
+		System.out.print("{");
+		for(int i = 0; i < tokens.length-2; i++){
+			System.out.print(tokens[i] + "," + tokens[i+1] + "," + tokens[i + 2] + ", ");
+		}
+		System.out.print("}");
+		System.out.println();
+	}
+		static void fourGram(String [] tokens){
+		System.out.println("//////////////");
+		System.out.println("3 Gram");
+		System.out.print("{");
+		for(int i = 0; i < tokens.length-3; i++){
+			System.out.print(tokens[i] + "," + tokens[i+1] + "," + tokens[i + 2] + "," + tokens[i + 3] + ", ");
+		}
+		System.out.print("}");
+		System.out.println();
+	}
+}
