3e7bcb72ca73888cac01e45af6e1dd7b4978f833
Stephen Cassedy <casseds95@gmail.com>
2017-02-27 20:50:27 +0000

    Attempt.java - More Efficient

--- lab3/TestPrep/Attempt.java
+++ lab3/TestPrep/Attempt.java
@@ -1,42 +1,43 @@
 import java.io.*;
 import java.util.ArrayList;
 
-class BleuScore{
+class Attempt{
   public static void main(String [] args){
     float bestnGramOne = 0;
     float bestnGramTwo = 0;
     float bestnGramThree = 0;
     float bestnGramFour = 0;
     float bestLengthMatch = 0;
-    String [] transArray = null;
-    String [] refArray = null;
     float brevity = 0;
+    String output = "";
+    String ref = "";
+    String [] outputArray = null;
+    String [] refArray = null;
+    ArrayList<String> refList = new ArrayList<>();
     try{
       /**Handle Files**/
-      BufferedReader br = new BufferedReader(new FileReader("Trans.txt"));
-      PrintWriter pw = new PrintWriter(new File("Output.txt"));
-      ArrayList<String> refList = new ArrayList<>();
-      String output = br.readLine();
+      BufferedReader br = new BufferedReader(new FileReader("trans.txt"));
+      PrintWriter pw = new PrintWriter("Output.txt");
+      output = br.readLine();
       System.out.println("Output : " + output);
       br = new BufferedReader(new FileReader("Refs.txt"));
-      String ref = "";
       while((ref = br.readLine()) != null){
-        refList.add(ref);
         System.out.println("Reference : " + ref);
+        refList.add(ref);
       }
       br.close();
       /**Main Calculations**/
       for(int i = 1; i < 5; i++){
         ArrayList<ArrayList<String>> nTransList = calculateNgrams(output, i);
         for(String s : refList){
-          transArray = output.split(" ");
+          outputArray = output.split(" ");
           refArray = s.split(" ");
-          if(transArray.length == refArray.length)
+          if(outputArray.length == refArray.length)
             bestLengthMatch = refArray.length;
-          else if(Math.abs(transArray.length - refArray.length) < Math.abs(transArray.length - bestLengthMatch))
+          else if(Math.abs(outputArray.length - refArray.length) < Math.abs(outputArray.length - bestLengthMatch))
             bestLengthMatch = refArray.length;
           ArrayList<ArrayList<String>> nRefList = calculateNgrams(s, i);
-          float precision = calcPrecision(nTransList, nRefList);
+          float precision = calculatePrecison(nTransList, nRefList);
           switch(i){
             case 1 :
               if(precision > bestnGramOne)
@@ -57,13 +58,12 @@ class BleuScore{
           }
         }
       }
-      /**If bestnGram(x) = 0, should I multiply the 0??**/
       float totalPrecision = bestnGramOne * bestnGramTwo * bestnGramThree * bestnGramFour;
-      if(transArray.length > bestLengthMatch)
-        brevity = bestLengthMatch / (float) transArray.length;
-      if(transArray.length < bestLengthMatch)
-        brevity = (float) transArray.length / bestLengthMatch;
-      if(transArray.length == bestLengthMatch)
+      if(outputArray.length > bestLengthMatch)
+        brevity = bestLengthMatch / (float) outputArray.length;
+      if(outputArray.length < bestLengthMatch)
+        brevity = (float) outputArray.length / bestLengthMatch;
+      if(outputArray.length == bestLengthMatch)
         brevity = 1;
       pw.println("Best 1 : " + bestnGramOne);
       pw.println("Best 2 : " + bestnGramTwo);
@@ -81,33 +81,28 @@ class BleuScore{
   }
 
   static double root(float n){
-    return Math.pow(n, 0.25f);
+    return Math.pow(n, 0.25);
   }
 
-  static float calcPrecision(ArrayList<ArrayList<String>> trans, ArrayList<ArrayList<String>> ref){
-    float precisionScore = calculatePercentDiff(trans, ref);
-    return precisionScore;
-  }
-
-  static float calculatePercentDiff(ArrayList<ArrayList<String>> trans, ArrayList<ArrayList<String>> ref){
-    float similarities = 0;
-    for(int i = 0; i < trans.size(); i++){
-      if(ref.contains(trans.get(i)))
-        similarities++;
+  static float calculatePrecison(ArrayList<ArrayList<String>> out, ArrayList<ArrayList<String>> ref){
+    float countSimilars = 0;
+    for(int i = 0; i < out.size(); i++){
+      if(ref.contains(out.get(i)))
+        countSimilars++;
     }
-    return similarities / (float) trans.size();
+    return countSimilars / (float) out.size();
   }
 
   static ArrayList<ArrayList<String>> calculateNgrams(String s, int n){
     ArrayList<ArrayList<String>> allSets = new ArrayList<>();
-    ArrayList<String> nGramSets = new ArrayList<>();
-    String [] splitString = s.split(" ");
-    for(int i = 0; i < splitString.length - (n-1); i++){
+    ArrayList<String> oneSet = new ArrayList<>();
+    String [] splitString  = s.split(" ");
+    for(int i = 0; i < splitString.length-(n-1); i++){
       for(int j = i; j < i + n; j++){
-          nGramSets.add(splitString[j]);
+        oneSet.add(splitString[j]);
       }
-      allSets.add(nGramSets);
-      nGramSets = new ArrayList<>();
+      allSets.add(oneSet);
+      oneSet = new ArrayList<>();
     }
     return allSets;
   }
