733856ca456df3ecd1da33baf05e2da6a1575c2a
Stephen Cassedy <casseds95@gmail.com>
2017-03-13 14:33:15 +0000

    Recursive N-Gram Function (Extra)

---
+++ lab4/TestPrep/RNgram.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+class RNgram{
+   public static void main(String[] args) {
+      ArrayList<ArrayList<String>> list = new ArrayList<>();
+      int index = 0;
+      String test = "the cat on the mat";
+      String [] array = test.split(" ");
+      ArrayList<ArrayList<String>> grams = new ArrayList<>();
+      for(int i = 1; i < 5; i++){
+         grams = gramSet(grams, array, i, index);
+      }
+      printList(grams);
+   }
+
+   static void printList(ArrayList<ArrayList<String>> list){
+      for(ArrayList<String> a : list){
+         System.out.print("{");
+         for(String s : a){
+            System.out.print(s + " ");
+         }
+         System.out.println("}");
+      }
+   }
+
+   static ArrayList<ArrayList<String>> gramSet(ArrayList<ArrayList<String>> list, String [] s, int n, int index){
+      if(index < s.length-(n-1)){
+         list.add(gram(s, n, index));
+         gramSet(list, s, n, index+=1);
+      }
+      return list;
+   }
+
+   static ArrayList<String> gram(String [] s, int n, int index){
+      ArrayList<String> set = new ArrayList<>();
+      for(int i = index; i < index + n; i++){
+         set.add(s[i]);
+      }
+      return set;
+   }
+}
