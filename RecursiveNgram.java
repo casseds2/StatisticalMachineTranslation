f35fc1c295141f19086700b71378791a5737d465
Stephen Cassedy <casseds95@gmail.com>
2017-03-13 15:00:15 +0000

    Recursive NGram Function (Extra)

--- RecursiveNgram.java
+++ RecursiveNgram.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-class RNgram{
+class RecursiveNgram{
    public static void main(String[] args) {
       ArrayList<ArrayList<String>> list = new ArrayList<>();
       int index = 0;
