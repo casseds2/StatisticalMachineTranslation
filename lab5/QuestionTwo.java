c5e97e15367e3fb61329a46c57d38ddc4dcc9a7d
Stephen Cassedy <casseds95@gmail.com>
2017-03-22 13:34:37 +0000

    Started QuestionTwo.java

--- lab5/QuestionTwo.java
+++ lab5/QuestionTwo.java
@@ -6,26 +6,54 @@ class QuestionTwo{
       String tarOne = "la maison";
       String srcTwo = "house";
       String tarTwo = "maison";
-      /**Makes Working With Values Easier**/
-      ArrayList<String> srcOneList = new ArrayList<>(Arrays.asList(srcOne.split(" ")));
-      ArrayList<String> tarOneList = new ArrayList<>(Arrays.asList(tarOne.split(" ")));
-      ArrayList<String> srcTwoList = new ArrayList<>(Arrays.asList(srcTwo.split(" ")));
-      ArrayList<String> tarTwoList = new ArrayList<>(Arrays.asList(tarTwo.split(" ")));
-      /**Get counts of the occurrences of word pairs**/
-      double t = 0;
-      for(String a : srcOneList){
-         for(String b : tarOneList){
-            t = getCount(a, srcOneList) / (double)tarOneList.size();
-            System.out.println("t(" + a + "|" + b + ") = " + t);
-         }
+      int iterNumber = 2;
+
+      /**Initialize t(e|f) uniformly**/
+      ArrayList<String> align = allAlignments(srcOne, tarOne);
+
+   }
+
+   /**Print a HashMap of type <String, Double>**/
+   static void printHashMap(HashMap<String, Double> hash){
+      for(Map.Entry<String, Double> entry : hash.entrySet()){
+         ArrayList<String> array = new ArrayList<>(Arrays.asList(entry.getKey().split(" ")));
+         System.out.println(entry.getKey() + " : " + entry.getValue());
       }
    }
 
-   static double getCount(String s, ArrayList<String> a){
-      double count = 0;
-      for(String q : a)
-         if(s.equals(q))
-            count+=1;
-      return count;
+   /**Add 2 ArrayLists of Strings to a HashMap and Return it**/
+   static HashMap<String, Double> initTvals(ArrayList<String> a, ArrayList<String> b){
+      HashMap<String, Double> tVals =  new HashMap<>();
+      addToHashMap(a, tVals);
+      addToHashMap(b, tVals);
+      return tVals;
+   }
+
+   static ArrayList<String> allAlignments(String srcOne, String tarOne){
+      String [] srcOneArray = srcOne.split(" ");
+      String [] tarOneArray = tarOne.split(" ");
+      ArrayList<String> src = new ArrayList<>();
+      src.addAll(Arrays.asList(srcOneArray));
+      ArrayList<String> tar = new ArrayList<>();
+      tar.addAll(Arrays.asList(tarOneArray));
+      ArrayList<String> used = new ArrayList<>();
+      for(String s : src){
+         for(String t : tar){
+            String align = s + " " + t;
+            if(!used.contains(align))
+               used.add(align);
+         }
+      }
+      return used;
+   }
+
+   /**Add a String ArrayList to a HashMap, catering for duplicates**/
+   static void addToHashMap(ArrayList<String> list, HashMap<String, Double> tVals){
+      for(String s : list){
+         if(tVals.containsKey(s))
+            tVals.put(s, tVals.get(s) + 1.0 / (double)list.size());
+         else
+            tVals.put(s, 1.0 / (double) list.size());
+      }
    }
 }
